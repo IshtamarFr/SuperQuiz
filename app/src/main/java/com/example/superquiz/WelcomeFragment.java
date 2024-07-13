8e49222507ebfd028b39779799b6ac3a33ec4f51
IshtamarFr <ishta@ishtamar.fr>
2024-07-13 10:40:05 +0200

    quizz layout is done correctly

--- app/src/main/java/com/example/superquiz/WelcomeFragment.java
+++ app/src/main/java/com/example/superquiz/WelcomeFragment.java
@@ -1,14 +1,23 @@
 package com.example.superquiz;
 
+import android.media.AudioManager;
+import android.media.ToneGenerator;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.superquiz.databinding.FragmentWelcomeBinding;
+
 public class WelcomeFragment extends Fragment {
+    private FragmentWelcomeBinding binding;
+
     public static WelcomeFragment newInstance() {
         WelcomeFragment fragment = new WelcomeFragment();
         return fragment;
@@ -20,9 +29,41 @@ public class WelcomeFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false);
+        binding=FragmentWelcomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.startButton.setEnabled(false);
+
+        binding.nameInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                binding.startButton.setEnabled(editable.length() > 0);
+            }
+        });
+
+        binding.startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ToneGenerator toneGen1 = new ToneGenerator(AudioManager.STREAM_MUSIC, 100);
+                toneGen1.startTone(ToneGenerator.TONE_CDMA_PIP,150);
+            }
+        });
     }
 }
